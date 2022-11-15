2066f745a7148c6eb939e8e1b23d6894b182becf
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-14 20:36:39 -0600

    Update index.js

--- jQuery/index.js
+++ jQuery/index.js
@@ -3,7 +3,7 @@
 //$("h1").before("<button>New</button>");
 //$("h1").after("<button>New</button>");
 //$("h1").prepend("<button>New</button>");
-
+//$("h1").append("<button>New</button>");
 
 // Usual way of adding an Event Listener
 //$(document).click(function(event) {
